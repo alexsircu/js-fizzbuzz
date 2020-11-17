17c028475ffbecca19eee61816d8b008da57811a
Alex Sircu <alex.sricu@gmail.com>
2020-11-17 17:14:10 +0100

    css

--- js/script.js
+++ js/script.js
@@ -6,16 +6,19 @@
 // Come faccio a sapere se un numero è divisibile per?
 // Abbiamo visto qualcosa di particolare che possiamo usare?
 
-for (var i = 1; i <= 100; i++) {
+for (var i = 1; i <= 1700; i++) {
 
   if  ((i % 3 == 0) && (i % 5 == 0)) {
+    document.getElementById("container").innerHTML += "FizzBuzz" + " ";
     console.log("FizzBuzz");
   } else if (i % 3 == 0) {
+    document.getElementById("container").innerHTML += "Fizz" + " ";
     console.log("Fizz");
   } else if (i % 5 == 0) {
+    document.getElementById("container").innerHTML += "Buzz" + " ";
     console.log("Buzz");
   } else {
+    document.getElementById("container").innerHTML += i + " ";
     console.log(i);
   }
-
 }
